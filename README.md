ea97205b54ee0de9aac31c87763ced2f5a7c0b14
Girija Joshi <girija_aj@yahoo.com>
2020-08-01 13:34:01 -0700

    Module-12 challenge

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # BellyButtonBiodiversity
-This project analyzes the data
+This project creates a dash board for belly button biacteria for provided sample
+### Summary
+* As soon as page loads it will take first ID from dorop down and populate all three chart defined below.
+* Project displays demographic information for the IDs. Acording to selected ID it shows bar graph of the top ten bacterial species in a volunteer’s navel.
+* Polts bubble chart for selected ID to visualize the relative frequency of all the bacterial species found in a volunteer’s navel.
+* Create gauge chart for selected ID's washing frequency.
+* To achive this goal we have used JavaScript, Plotly and HTML
+### Example
+#### 940 
+![940_all](images/940_all.png)
+#### 946 
+![946_all](images/946_all.png)
+#### 960 
+![960_all](images/960_all.png)
---
+++ plots.js
@@ -0,0 +1,247 @@
+var jsonFileName = "samples.json";
+
+/* This function fills dropdown menu values from dataset and draws appropriate charts */
+function init() {
+    var selector = d3.select("#selDataset");
+    var options;
+    var r;
+    d3.json(jsonFileName).then((data) => {
+      console.log(data);
+      var sampleNames = data.names;
+      sampleNames.forEach((sample) => {
+        selector
+          .append("option")
+          .text(sample)
+          .property("value", sample);
+      });
+      let e = document.getElementById("selDataset");
+      r = e.options[0].value;
+      console.log("E: " + r);
+      optionChanged(r);
+    });
+}
+
+/*
+  This funtion gets value from json file then gets metadata dictionary
+  and gets values of each element from dictionary and then prints 
+  it to the panel.
+*/
+function buildMetadata(sample) {
+  d3.json(jsonFileName).then((data) => {
+      let title = "";
+      var metadata = data.metadata;
+      var resultArray  = metadata.filter(sampleObj => sampleObj.id == sample);
+      var result = resultArray[0];
+      console.log(result);
+      var PANEL = d3.select("#sample-metadata");
+      PANEL.html("");
+      title = "ID: ";
+      PANEL.append("h6").text(title + result.id);
+      title = "ETHNICITY: ";
+      PANEL.append("h6").text(title + result.ethnicity);
+      title = "GENDER: ";
+      PANEL.append("h6").text(title + result.gender);
+      title = "AGE: ";
+      PANEL.append("h6").text(title + result.age);
+      title = "LOCATION: ";
+      PANEL.append("h6").text(title + result.location);
+      title = "BBTYPE: ";
+      PANEL.append("h6").text(title + result.bbtype);
+      title = "WFREQ: ";
+      PANEL.append("h6").text(title + result.wfreq);
+  });
+}
+
+/*
+  This function gets data from json file and then for selected sample ID gets other information
+  and calls other graph functions to plot the data.
+*/
+function buildCharts(sample) {
+  var samples;
+  d3.json(jsonFileName).then((data) => {
+    // getting metadata dictionary to get wfreq value for guage chart
+    var metadata = data.metadata;
+    var resultArrayMeta  = metadata.filter(sampleObj => sampleObj.id == sample);
+    var resultMeta = resultArrayMeta[0];
+    console.log("WFRQ: " + resultMeta.wfreq);
+    
+    // getting samples for selected id to get 10 sample values and corrospoing
+    // otu labels
+    samples = data.samples;
+    var resultArray  = samples.filter(sampleObj => sampleObj.id == sample);
+    var result = resultArray[0];
+    console.log(result);
+
+    let otu_ids = result.otu_ids;
+    let firstTenOtu = otu_ids.slice(0,10);
+
+    let sampleValues = result.sample_values;
+    let firstTenSamples = sampleValues.slice(0,10);
+
+    let otuLabels = result.otu_labels;
+
+    let firstTenOtuLable = firstTenOtu.map(otu => "OTU " + otu);
+
+    buildBarChart(firstTenSamples, firstTenOtuLable, sample);
+    buildBubbleChart(otu_ids, sampleValues, otuLabels)
+    buildGaugeChart(resultMeta.wfreq);
+  });
+
+}
+
+
+/*
+  This function plots bar chart with first 10 samples and corrosponding lables.
+*/
+function buildBarChart(firstTenSamples, firstTenOtuLable, sample) {
+  var trace = {
+    x: firstTenSamples.reverse(),
+    y: firstTenOtuLable.reverse(),
+    type: "bar",
+    orientation: 'h'
+  };
+
+  var data = [trace];
+  var layout = {
+    title: "OTU for " + sample,
+    xaxis: { title: "Sample Values"},
+    yaxis: { title: "OTU IDs"}
+  };
+  Plotly.newPlot("bar", data, layout);
+}
+
+/*
+  This function plots bubble chart for sample_values for selected id
+*/
+function buildBubbleChart(otu_ids, sampleValues, otuLabels) {
+  var trace = {
+    x: otu_ids,
+    y: sampleValues,
+    text: otuLabels,
+    mode: 'markers',
+    marker: {
+      color: otu_ids,
+      size: sampleValues,
+      colorscale: 'Earth'
+    }
+  };
+
+  var data = [trace];
+  var layout = {
+    showlegend: false,
+    height: 500,
+    width: 1000
+  };
+  
+  Plotly.newPlot('bubble', data, layout);
+}
+
+function buildGaugeChart(wfreq) {
+  // Enter a speed between 0 and 180
+  var level = parseFloat(wfreq) * 20;
+  console.log("LEVEL: " + level);
+
+  /*
+    This code to calculate needle and its position.
+  */
+  // Trig to calc meter point
+  var degrees = 180 - level;
+  console.log("DEGREES: "+ degrees);
+
+  radius = .5;
+  var radians = (degrees * Math.PI) / 180;
+  console.log("RADIANCE: " + radians);
+
+  var x = radius * Math.cos(radians);
+  var y = radius * Math.sin(radians);
+
+  console.log('X angle: ' + x);
+  console.log('Y angle: ' + y);
+  console.log('DEGREES: ' + degrees);
+
+  var path1 = (degrees < 45 || degrees > 135) ? 'M -0.0 -0.025 L 0.0 0.025 L ' : 'M -0.025 -0.0 L 0.025 0.0 L ';
+  console.log("PATH 1: " + path1)
+  
+  var mainPath = path1,
+      pathX = String(x),
+      space = ' ',
+      pathY = String(y),
+      pathEnd = ' Z';
+
+  var path = mainPath.concat(pathX,space,pathY,pathEnd);
+  console.log("PATH: " + path);
+
+  var data = [
+    { 
+      type: 'scatter',
+      x: [0], 
+      y:[0],
+      marker: {size: 14, color:'850000'},
+      showlegend: false,
+      name: 'Freq',
+      text: level,
+      hoverinfo: 'text+name'
+    },
+    { 
+      values: [50 / 9, 50 / 9, 50 / 9, 50 / 9, 50 / 9, 50 / 9, 50 / 9, 50 / 9, 50 / 9, 50],
+      rotation: 90,
+      text: ['8-9','7-8','6-7','5-6','4-5','3-4','2-3','1-2','0-1',''],
+      textinfo: 'text',
+      textposition:'inside',
+      marker: 
+      {
+        colors:[
+          "rgba(0, 105, 11, .5)",
+          "rgba(10, 120, 22, .5)",
+          "rgba(14, 127, 0, .5)",
+          "rgba(110, 154, 22, .5)",
+          "rgba(170, 202, 42, .5)",
+          "rgba(202, 209, 95, .5)",
+          "rgba(210, 206, 145, .5)",
+          "rgba(232, 226, 202, .5)",
+          "rgba(240, 230, 215, .5)",
+          "rgba(255, 255, 255, 0)"
+        ]
+      },
+      labels: ["8-9", "7-8", "6-7", "5-6", "4-5", "3-4", "2-3", "1-2", "0-1", ""],
+      hoverinfo: 'label',
+      hole: .5,
+      type: 'pie',
+      showlegend: false
+    }
+  ];
+
+  var layout = {
+    shapes:[
+      {
+        type: 'path',
+        path: path,
+        fillcolor: '850000',
+        line: {
+          color: '850000'
+        }
+      }
+    ],
+    title: "<b>Belly Button Washing Frequency</b> <br> Scrubs per Week",
+    height: 400,
+    width: 400,
+    xaxis: {
+      zeroline:false, showticklabels:false,
+      showgrid: false, range: [-1, 1]
+    },
+    yaxis: {
+      zeroline:false, showticklabels:false,
+      showgrid: false, range: [-1, 1]
+    }
+  };
+
+  Plotly.newPlot('gauge', data, layout);
+}
+
+function optionChanged(newSample) {
+  console.log(newSample);
+  buildMetadata(newSample);
+  buildCharts(newSample);
+}
+
+init();
